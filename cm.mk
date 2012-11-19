d3e6e6d4cf78560bae47f4989eabc80409afc1c3
Hadi Sutedjo <x1123589@gmail.com>
2012-11-19 11:37:39 +0000

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,23 +1,23 @@
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
 # Release name
-PRODUCT_RELEASE_NAME := DV
+PRODUCT_RELEASE_NAME := Thunder
 
 TARGET_BOOTANIMATION_NAME := vertical-400X800
-
-# Inherit AOSP device configuration for VENUE.
-$(call inherit-product, device/dell/venue/venue.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+# Inherit AOSP device configuration for VENUE.
+$(call inherit-product, device/dell/venue/venue.mk)
 
 #
 # Setup device specific product configuration.
 #
-PRODUCT_DEVICE := venue
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0  
 PRODUCT_NAME := cm_venue
+PRODUCT_DEVICE := venue
 PRODUCT_BRAND := venue
 PRODUCT_MODEL := Dell-Venue
 PRODUCT_MANUFACTURER := DELL
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=cm_venue BUILD_ID=IML74K BUILD_DISPLAY_ID=IML74K BUILD_FINGERPRINT="dell/venue/venue:4.0.2/ICL53F/235179:user/release-keys" PRIVATE_BUILD_DESC="cm_venue 4.0.2 ICL53F 235179 release-keys"
